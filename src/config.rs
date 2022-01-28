83d1b1a56e4becdcb7a15eae4a58dd912e7f3ff8
Marek Veselý <veselym@spstrutnov.cz>
2022-01-28 19:42:25 +0100

    Create configuration and actix main file

---
+++ src/config.rs
@@ -0,0 +1,32 @@
+use serde::Deserialize;
+use std::path::PathBuf;
+
+#[derive(Deserialize, Clone)]
+pub struct Config {
+    pub address: String,
+    pub port: u16,
+    pub jwt: JWTConfig,
+    pub info: SiteConfig,
+    #[serde(default)]
+    pub cors: Option<CORSConfig>,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct JWTConfig {
+    pub valid_for: i64,
+    pub private_key: PathBuf,
+    pub public_key: PathBuf,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct SiteConfig {
+    pub storage_url: String,
+    pub name: String,
+    pub version: String,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct CORSConfig {
+    #[serde(rename = "allowed_origins")]
+    pub origins: Vec<String>,
+}
