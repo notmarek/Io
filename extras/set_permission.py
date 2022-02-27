58c325381ef6bb008250e16bf5575d99c7944269
Marek Veselý <veselym@spstrutnov.cz>
2022-02-27 15:56:24 +0100

    add a python script to set user permissions

---
+++ extras/set_permission.py
@@ -0,0 +1,18 @@
+import psycopg2
+import json
+
+with open("config.json") as f:
+    conn_string = json.loads(f.read())["db"]["connection_string"]
+
+username = input("Username: ")
+permissions = input("Permissions (comma separated): ").split(",")
+
+conn = psycopg2.connect(conn_string)
+
+cur = conn.cursor()
+
+cur.execute(f"""UPDATE users SET permissions = '{{{",".join(permissions)}}}' WHERE username = '{username}'""")
+
+cur.execute(f"""SELECT * FROM users WHERE username = '{username}'""")
+rows = cur.fetchall()
+print(rows)
